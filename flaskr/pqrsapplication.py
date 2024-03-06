1e74cee75ff95f343c694357ba13d65509c04d60
Juan Huguet <jhuguetospina@gmail.com>
2024-03-05 21:55:56 -0500

    ADD: imports and dynamic methods

--- flaskr/pqrsapplication.py
+++ flaskr/pqrsapplication.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
+from sqlstatements import *
+from utils import *
 import json
 
 app = Flask(__name__)
@@ -12,7 +14,7 @@ mysql = MySQL(app)
 
 @app.route("/")
 def home():
-    return render_template('pqrs_corpus.html', logged=_is_logged(), message="This is my landing page at home!")
+    return render_template('pqrs_corpus.html', logged=True, message="This is my landing page at home!")
 
 @app.route('/data/<string:table_name>/<int:id>', methods=["GET"])
 def get_single_entry(table_name, id):
@@ -22,19 +24,14 @@ def get_single_entry(table_name, id):
     table_name: Gives the specific table name
     id: Gives the specific ID to look for in the database
     """
-    cur = mysql.connection.cursor()
-    cur.execute('''SELECT * FROM {table} WHERE {table}_id = %s'''.format(table = table_name), (id,))
-    data = cur.fetchall()
-    cur.close()
-    passdown = {
-        "logged" : True,
-        "table_information" : True,
-        "response_table" : data,
-        "table_name" : table_name
-    }
-    return render_template('pqrs_corpus.html', data = passdown)
-
-    return jsonify(data).get_json
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute('''SELECT * FROM {table} WHERE {table}_id = %s'''.format(table = table_name), (id,))
+        data = cur.fetchall()
+        cur.close()
+        return render_template('pqrs_corpus.html', data = passdown_response(True, True, data, table_name))
+    except:
+        return render_template('pqrs_corpus.html', logged=True,  data = passdown_empty())
 
 @app.get('/data/<string:table_name>')
 def get_all(table_name):
@@ -43,22 +40,38 @@ def get_all(table_name):
 
     table_name: Gives the specific table name
     """
-
     try:
         cur = mysql.connection.cursor()
         cur.execute('''SELECT * FROM {table}'''.format(table = table_name))
         data = cur.fetchall()
         cur.close()
-        passdown = {
-            "logged" : True,
-            "table_information" : True,
-            "response_table" : data,
-            "table_name" : table_name
-        }
-        return render_template('pqrs_corpus.html', data = passdown)
+        return render_template('pqrs_corpus.html', data = passdown_response(True, True, data, table_name))
     except:
-        return render_template('pqrs_corpus.html', logged=True, tableinformation = False, response_table = None)
+        return render_template('pqrs_corpus.html', logged=True,  data = passdown_empty())
 
+@app.post("/new_person")
+def insert_new_person():
+    try:
+        request_data = json.loads(request.data.decode('utf-8'))
+        cur = mysql.connection.cursor()
+        cur.execute(create_new_person(), create_statements_block(request_data))
+        mysql.connection.commit()
+        cur.close()
+    except Exception as e:
+        return e
 
-def _is_logged():
-    return True
+    return "Insert successfull"
+
+@app.post("/new_role")
+def insert_new_role():
+    try:
+        request_data = json.loads(request.data.decode('utf-8'))
+        cur = mysql.connection.cursor()
+        cur.execute(create_new_role(), create_statements_block(request_data))
+        mysql.connection.commit()
+        cur.close()
+    except Exception as e:
+        return e
+
+    return "Insert successfull"
+
