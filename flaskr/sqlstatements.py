74629f74502aa1e112c62d79606ded6c9ef70629
Juan Huguet <jhuguetospina@gmail.com>
2024-03-05 21:55:03 -0500

    ADD: generic statements

---
+++ flaskr/sqlstatements.py
@@ -0,0 +1,11 @@
+def create_new_role():
+    return """INSERT INTO role (name) VALUES (%s)"""
+
+def create_new_person():
+    return """INSERT INTO person (name, document_id, email, position_id, role_id, department_id) VALUES (%s, %s, %s, %s, %s, %s)"""
+
+def create_statements_block(request_data):
+    parameters = []
+    for key, value in request_data.items():
+        parameters.append(value)
+    return tuple(parameters)
