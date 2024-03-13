0c588ae1c628caae360197f4da3ec96cd63c7249
Juan Huguet <jhuguetospina@gmail.com>
2024-03-12 22:10:24 -0500

    ADD: instance returning

--- flaskr/domain/config.py
+++ flaskr/domain/config.py
@@ -1,6 +1,12 @@
+from flask_mysqldb import MySQL
+_mysql = MySQL()
+
 class Config:
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'password'
     MYSQL_DB = 'pqrs'
     MYSQL_CURSORCLASS = 'DictCursor'
+
+    def give_mysql_instance():
+        return _mysql
