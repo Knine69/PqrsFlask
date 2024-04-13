2067f77510dda59e1698491e495b843bee99edf5
Juan Huguet <jhuguetospina@gmail.com>
2024-04-13 16:16:02 -0500

    Add sonar in docker

---
+++ sonar-docker.sh
@@ -0,0 +1,1 @@
+docker run -d --name sonarqube -p 9000:9000 -v sonarqube_data:/opt/sonarqube/data -v sonarqube_extensions:/opt/sonarqube/extensions -v sonarqube_logs:/opt/sonarqube/logs sonarqube:9.9.4-community
