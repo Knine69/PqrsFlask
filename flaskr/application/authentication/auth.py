6dd45034ae922427dc7bc3549bfc6ba335eb0b95
Juan Huguet <jhuguetospina@gmail.com>
2024-04-18 15:43:46 -0500

    ADD. authentication manager

--- flaskr/application/authentication/auth.py
+++ flaskr/application/authentication/auth.py
@@ -1,15 +1,33 @@
+from ..dto.user import UserDto
+from ...domain.models.queries.personquery import get_person_by_document_id
+from ...domain.models.person import Person
 class AuthenticationManager():
-    def __init__(self) -> None:
-        pass
+    _mysql = None 
 
-    def authenticate():
-        pass
+    def __init__(self, mysql) -> None:
+        self._mysql = mysql
+        
+    def authenticate(self, request_data):
+        user_dto = UserDto(request_data["document"], request_data["password"])
+        person = Person(self._get_user_by_document(user_dto.get_document()))
+        if self._login(user_dto.get_password(), person.get_password()):
+            return "Correctly authenticated"
+        return "Authentication failed" 
 
-    def get_user_by_document():
-        pass
+    def authorize_operation(self, user_dto, token):
+        return self._validate_credentials(user_dto, token)
+    
 
-    def validate_credentials():
-        pass
+    def _get_user_by_document(self, document) -> dict: 
+        response = get_person_by_document_id(self._mysql, document)["person"]
+        return response if response else None
+        
+    def _validate_credentials(self, user_dto, token) -> bool:
+        # Check case for JWT validation
+        return 
 
-    def _compare_password():
-        pass
+    def _login(self, dto_password, password):
+        return self._compare_password(dto_password, password)
+
+    def _compare_password(self, dto_password:str, password:str):
+        return dto_password.lower() == password.lower()
