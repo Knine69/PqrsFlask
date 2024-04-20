d2a614ac81dde29cc144984b4402528ad288a328
Juan Huguet <jhuguetospina@gmail.com>
2024-04-20 11:38:10 -0500

    REFACTOR: unnecessary methods and variable initialization

--- flaskr/application/security/tokenmanager.py
+++ flaskr/application/security/tokenmanager.py
@@ -1,20 +1,18 @@
 from datetime import datetime, timedelta, timezone
 from ...domain.models.person import Person
 from ...application.dto.user import UserDto
-from jose import jwt
-from jose.constants import ALGORITHMS
+from ...domain.models.queries.utils.secureutilities import SecurityConstants
+from jose import jwt 
+from jose.constants import ALGORITHMS 
 
-SECRET_KEY_PATH = "application/security/rsa/private_key.key"
-PUBLIC_KEY_PATH = "application/security/rsa/public_key.key"
 
 class JwtManager():
-    __secret_key = None
-    __public_key = None
+    __security_constants = SecurityConstants()
     def __init__(self) -> None:
-        with open(SECRET_KEY_PATH, 'r') as file:
+        with open(self.__security_constants.get_secret_key_path(), 'r') as file:
             self.__secret_key = file.read().strip()
 
-        with open(PUBLIC_KEY_PATH, 'r') as file:
+        with open(self.__security_constants.get_public_key_path(), 'r') as file:
             self.__public_key = file.read().strip()
 
     def generate_token(self, user_details: Person):
@@ -34,6 +32,3 @@ class JwtManager():
             return payload
 
         return "You're not who you say you are!"
-
-    def __get_secret_key(self):
-        return self.__secret_key
