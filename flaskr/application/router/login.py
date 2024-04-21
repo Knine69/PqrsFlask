d38dfad12220afc2804710415594ddc0a2c2ef6b
Juan Huguet <jhuguetospina@gmail.com>
2024-04-21 18:56:39 -0500

    DELETE: passdown parameter

--- flaskr/application/router/login.py
+++ flaskr/application/router/login.py
@@ -11,5 +11,5 @@ mysql = Config.give_mysql_instance(self=Config)
 @router_login.post("/auth")
 def login():
     request_data = json.loads(request.data.decode('utf-8'))
-    auth_manager = AuthenticationManager(mysql)
+    auth_manager = AuthenticationManager()
     return auth_manager.authenticate(request_data)
