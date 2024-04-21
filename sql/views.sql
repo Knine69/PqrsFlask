06ee3ad1408d9e267eae5a5a1d136ea7796ce663
Juan Huguet <jhuguetospina@gmail.com>
2024-04-21 18:56:07 -0500

    ADD: new query to see person requests

--- sql/views.sql
+++ sql/views.sql
@@ -25,3 +25,18 @@ FROM person p INNER JOIN position o ON p.position_id = o.position_id
 INNER JOIN role r ON p.role_id = r.role_id
 INNER JOIN department d ON d.department_id = p.department_id
 WHERE p.person_id = get_person_id();
+
+CREATE OR REPLACE VIEW LookUpPersonRequests AS
+SELECT 
+r.request_id as 'RequestId',
+r.generated_at as 'Timestamp',
+r.summary as 'Summary',
+c.name AS 'Category',
+s.name as 'State',
+q.name as 'Solver'
+FROM person p INNER JOIN request r ON r.requester_id = p.person_id
+INNER JOIN state s ON r.state_id = s.state_id
+INNER JOIN person q ON r.solver_id = q.person_id
+INNER JOIN category c on r.category_id = c.category_id
+WHERE p.person_id = get_person_id() 
+ORDER BY r.generated_at;
