243497040a367a4c37b4d7db1c568eb5fac202fe
Juan Huguet <jhuguetospina@gmail.com>
2024-04-26 12:20:54 -0500

    REFACTOR: view consulted

--- flaskr/domain/models/queries/requestquery.py
+++ flaskr/domain/models/queries/requestquery.py
@@ -1,6 +1,6 @@
 from .queryexecutor import QueryExecutor
 from ....application.router.utils.utils import ERROR_MESSAGE
-from ..sqlstatements import create_statements_block, create_new_request, get_person_requests, get_one_from_table
+from ..sqlstatements import create_statements_block, create_new_request, get_person_requests, get_one_from_table, get_request_information
 
 from ....application.router.utils.utils import get_person_id_by_document_id, get_category_id_by_name, give_new_request_body
 from ....application.security.tokenmanager import JwtManager
@@ -23,7 +23,7 @@ class Request(QueryExecutor):
         elif validation["userValidated"]:
             person_id = self._get_person(document)["person_id"]
             with self.mysql.connection.cursor() as cur:
-                cur.execute(get_one_from_table().format(self.table_name, self.table_name), create_statements_block({"id": id}))
+                cur.execute(get_request_information(), (id, ))
                 data = cur.fetchall()
                 cur.close()
                 return data if data[0]["requester_id"] == person_id else (super().return_unauthorized_error() )
