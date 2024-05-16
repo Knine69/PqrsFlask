ce746c46ccd962358fadc0e08ab87d7faf07e1f2
Juan Huguet <jhuguetospina@gmail.com>
2024-05-15 21:44:46 -0500

    ADD: mail connection configuration

--- flaskr/domain/config.py
+++ flaskr/domain/config.py
@@ -1,12 +1,28 @@
 from flask_mysqldb import MySQL
+from flask_mail import Mail
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 class Config:
     _mysql = MySQL()
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'password'
-    MYSQL_DB = 'pqrs'
+    _mail = Mail()
+
+    MYSQL_HOST = os.environ.get('MYSQL_HOST')
+    MYSQL_USER = os.environ.get('MYSQL_USER')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+    MYSQL_DB = os.environ.get('MYSQL_DB')
     MYSQL_CURSORCLASS = 'DictCursor'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS = True
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
     def give_mysql_instance(self):
         return self._mysql
+    
+    def give_mail_instance(self):
+        return self._mail
